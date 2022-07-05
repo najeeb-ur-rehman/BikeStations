939dadc7c40313eb7dd699a81e0dc32baef4a1de
Najeeb <najeeb.rehman@digitify.com>
2022-07-05 22:37:30 +0500

    Initial Commit

---
+++ BikeStations/BikeStationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeStationsApp.swift
+//  BikeStations
+//
+//  Created by Najeeb on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BikeStationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
